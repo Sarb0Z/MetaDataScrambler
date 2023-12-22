7cd16355dd958080bc46f8a04722eafc57f698bd
Sarb0z <93070907+Sarb0Z@users.noreply.github.com>
2023-12-22 08:04:18 -0500

    init commit

---
+++ app/src/main/java/com/sarb0z/scrambler/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.sarb0z.scrambler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sarb0z.scrambler.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}

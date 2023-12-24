e921bcb8f8643034f8fbf95d0700080a78180f08
Sarb0z <93070907+Sarb0Z@users.noreply.github.com>
2023-12-24 11:32:18 -0500

    added workmanager instance to run in background

--- app/src/main/java/com/sarb0z/scrambler/MainActivity.kt
+++ app/src/main/java/com/sarb0z/scrambler/MainActivity.kt
@@ -9,6 +9,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Schedule the worker to run periodically
+        PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(
+            MediaScramblerWorker.class,
+            24, TimeUnit.HOURS)
+            .build();
+
+        WorkManager.getInstance(this).enqueue(periodicWorkRequest);
+
+        // TODO: Implement logic to detect when a new file is downloaded and enqueue the worker
+        // For example, use a BroadcastReceiver to listen for DOWNLOAD_COMPLETE broadcasts
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())

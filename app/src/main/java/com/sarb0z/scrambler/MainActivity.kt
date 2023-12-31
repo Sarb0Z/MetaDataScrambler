88f26950850756d217a80eb99ef04927cdd489bd
Sarb0z <93070907+Sarb0Z@users.noreply.github.com>
2023-12-31 10:39:49 -0500

    Implement logic to detect when a new file is downloaded and enqueue the worker

--- app/src/main/java/com/sarb0z/scrambler/MainActivity.kt
+++ app/src/main/java/com/sarb0z/scrambler/MainActivity.kt
@@ -6,6 +6,8 @@ import com.sarb0z.scrambler.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private static final String DOWNLOAD_COMPLETE_ACTION = "android.intent.action.DOWNLOAD_COMPLETE";
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,7 +19,8 @@ class MainActivity : AppCompatActivity() {
             .build();
 
         WorkManager.getInstance(this).enqueue(periodicWorkRequest);
-
+        // Register the BroadcastReceiver to listen for DOWNLOAD_COMPLETE broadcasts
+        registerDownloadCompleteReceiver();
         // TODO: Implement logic to detect when a new file is downloaded and enqueue the worker
         // For example, use a BroadcastReceiver to listen for DOWNLOAD_COMPLETE broadcasts
         if (savedInstanceState == null) {
@@ -25,5 +28,25 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+    private void registerDownloadCompleteReceiver() {
+        IntentFilter intentFilter = new IntentFilter(DOWNLOAD_COMPLETE_ACTION);
+        BroadcastReceiver downloadCompleteReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // Handle the download complete event
+                enqueueMediaOrganizerWorker();
+            }
+        };
+        registerReceiver(downloadCompleteReceiver, intentFilter);
+    }
+
+    private void enqueueMediaOrganizerWorker() {
+        // Enqueue the MediaOrganizerWorker when a new file is downloaded
+        OneTimeWorkRequest downloadCompleteWorkRequest =
+                new OneTimeWorkRequest.Builder(MediaOrganizerWorker.class)
+                        .build();
+
+        WorkManager.getInstance(this).enqueue(downloadCompleteWorkRequest);
     }
 }
